5f033a0b01c86831843a8d814cfe6dda6b1b75d1
Yossi Kuperman <yossiku@mellanox.com>
2018-05-06 09:41:29 +0300

    Introduce is_tcf_ct
    Change-Id: Idd40fe6f8abfa68dd5d1b8336e5d7eb829fb9075
    Signed-off-by: Yossi Kuperman <yossiku@mellanox.com>

--- include/net/tc_act/tc_ct.h
+++ include/net/tc_act/tc_ct.h
@@ -3,6 +3,7 @@
 #define __NET_TC_CT_H
 
 #include <net/act_api.h>
+#include <uapi/linux/tc_act/tc_ct.h>
 
 struct tcf_conntrack_info {
 	struct tc_action common;
@@ -11,5 +12,14 @@ struct tcf_conntrack_info {
 };
 
 #define to_conntrack(a) ((struct tcf_conntrack_info *)a)
+
+static inline bool is_tcf_ct(const struct tc_action *a)
+{
+#ifdef CONFIG_NET_CLS_ACT
+	if (a->ops && a->ops->type == TCA_ACT_CONNTRACK)
+		return true;
+#endif
+	return false;
+}
 
 #endif /* __NET_TC_CT_H */
