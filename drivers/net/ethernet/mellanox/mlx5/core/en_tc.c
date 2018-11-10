d9273b605a9e991223c0cdfb729aab9caf7d6b0e
Alex <jianglidong3@jd.com>
2018-11-10 18:08:13 +0800

    If microflow_register_ct_flow fails, as part of cleanup, remove microflow from parent flows before free microflow.

--- drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
@@ -3394,6 +3394,17 @@ static void microflow_init(struct mlx5e_microflow *microflow,
 	memset(microflow->path.cookies, 0, sizeof(microflow->path.cookies));
 }
 
+static void microflow_dettach(struct mlx5e_microflow *microflow)
+{
+	int i;
+
+	/* remove self from all parent flows */
+	for (i=0; i<microflow->nr_flows; i++) {
+		if (!list_empty_careful(&microflow->mnodes[i].node))
+			list_del(&microflow->mnodes[i].node);
+	}
+}
+
 static void microflow_attach(struct mlx5e_microflow *microflow)
 {
 	struct mlx5e_tc_flow *flow;
@@ -3504,10 +3515,17 @@ static int __microflow_merge(struct mlx5e_microflow *microflow)
 	if (err && err != -EAGAIN)
 		goto err;
 
+	/* Process EAGAIN as other errors before one solution is ready */
+	if (err == EAGAIN) {
+		etrace(" mflow: %px, hit EAGAIN in configure_fdb, mflow->rule[0]=%px", mflow, mflow->rule[0]);
+		goto err;
+	}
+
 	microflow_attach(microflow);
 
 	err = microflow_register_ct_flow(microflow);
 	if (err) {
+		microflow_dettach(microflow);
 		mlx5e_tc_del_flow(priv, mflow);
 		kfree(mflow);
 		goto err_flow;
